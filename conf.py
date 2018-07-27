efb67fc3e57a717ae7023c40ce84267b76506851
A Torin <alberto.torin@gmail.com>
2018-07-27 16:53:25 +0200

    Allegato

--- conf.py
+++ conf.py
@@ -226,7 +226,7 @@ latex_elements = {
 #'pointsize': '10pt',
 
 # Additional stuff for the LaTeX preamble.
-#'preamble': '',
+'preamble': '\\renewcommand\\appendixname{Allegato}',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
